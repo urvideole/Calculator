e85b7a25e6a5a34ba2fb0c01ebe260f7a9d835bf
Urvi <urvideole900@gmail.com>
2023-06-11 14:38:35 +0530

    calculator using react

---
+++ src/OperationButton.js
@@ -0,0 +1,11 @@
+import { ACTIONS } from "./App";
+
+function OperationButton({operation, dispatch}) {
+    return (
+        <button onClick={ () => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: {operation} }) }>
+            {operation}
+        </button>
+    )
+}
+
+export default OperationButton;
